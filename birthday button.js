8e615875d64da8f63bc02e10990e95a526dacab3
reenaamanjeetsingh-sys <reenaamanjeetsingh@gmail.com>
2026-06-05 15:25:24 +0530

    first commit

---
+++ birthday button.js
@@ -0,0 +1,10 @@
+function showMessage() {
+    document.getElementById("msg").style.display = "block";
+}
+function onemoresurprise() {
+    document.getElementById("drawingSection").style.display = "block";
+}
+
+function showFinalMessage() {
+    document.getElementById("finalMsg").style.display = "block";
+}
